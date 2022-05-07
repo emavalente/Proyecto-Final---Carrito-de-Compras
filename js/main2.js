fe6dd42ef411d160276e47ff06d4c789d81806fe
Emanuel Valente <ema_zero@hotmail.com>
2022-05-07 00:23:05 -0300

    07/05/22 - probando pre-loader

--- js/main2.js
+++ js/main2.js
@@ -1,32 +1,31 @@
 window.addEventListener('load', () => {
     const loader = document.getElementById('loader');
+    console.log(loader);
     setTimeout(() => {
         loader.classList.toggle('loaderExit');
     }, 3000);
-    
 
+    // DECLARACION DE VARIABLES.
+    let listaCarrito = [];
+    let contador = 0;
+    let subTotal = 0;
+    let nubeCarrito;
+    let nubeRecuperada;
+    let listaProductos = [];
 
-// DECLARACION DE VARIABLES.
-let listaCarrito = [];
-let contador = 0;
-let subTotal = 0;
-let nubeCarrito;
-let nubeRecuperada;
-let listaProductos = [];
+    // ACCIONES DEL MODAL CARRITO:
+    const btnModal = document.getElementById('btn-carrito');
+    const ventanaModal = document.getElementById('modal');
 
-// ACCIONES DEL MODAL CARRITO:
-const btnModal = document.getElementById('btn-carrito');
-const ventanaModal = document.getElementById('modal');
+    //Mostrar y ocultar el modal Carrito.
+    btnModal.addEventListener('click', () => {
+        ventanaModal.classList.toggle('modal-show'); // toggle intercambia entre clases.
+    });
 
-//Mostrar y ocultar el modal Carrito.
-btnModal.addEventListener('click', () => {
-    ventanaModal.classList.toggle('modal-show'); // toggle intercambia entre clases.
-});
-
-//Armado Inicial del Modal.
-const div = document.createElement('div');
-div.className = 'modalCarrito';
-div.innerHTML = `
+    //Armado Inicial del Modal.
+    const div = document.createElement('div');
+    div.className = 'modalCarrito';
+    div.innerHTML = `
     <div class="titulo-carrito">
         <h3>Lista de Productos</h3>
     </div>
@@ -41,11 +40,11 @@ div.innerHTML = `
     </div>
 `;
 
-ventanaModal.appendChild(div);
+    ventanaModal.appendChild(div);
 
-const modalCompra = document.createElement('div');
-modalCompra.className = 'modalCarrito carrito-show';
-modalCompra.innerHTML = `
+    const modalCompra = document.createElement('div');
+    modalCompra.className = 'modalCarrito carrito-show';
+    modalCompra.innerHTML = `
     <div class="titulo-carrito">
         <h3>Compra a realizar</h3>
     </div>
@@ -62,54 +61,54 @@ modalCompra.innerHTML = `
     </div>
     `;
 
-ventanaModal.appendChild(modalCompra);
+    ventanaModal.appendChild(modalCompra);
 
 
-//Boton Vaciar Carrito.
-const btnVaciar = document.getElementById('btn-vaciar');
-btnVaciar.addEventListener('click', vaciarCarrito);
+    //Boton Vaciar Carrito.
+    const btnVaciar = document.getElementById('btn-vaciar');
+    btnVaciar.addEventListener('click', vaciarCarrito);
 
 
-//Boton Comprar.
-const btnComprar = document.getElementById('btn-comprar');
-btnComprar.addEventListener('click', () => {
-    div.classList.toggle('carrito-show');
-    modalCompra.classList.toggle('carrito-show');
-});
+    //Boton Comprar.
+    const btnComprar = document.getElementById('btn-comprar');
+    btnComprar.addEventListener('click', () => {
+        div.classList.toggle('carrito-show');
+        modalCompra.classList.toggle('carrito-show');
+    });
 
-//Boton Volver.
-const btnVolver = document.getElementById('btn-volver');
-btnVolver.addEventListener('click', () => {
-    div.classList.toggle('carrito-show');
-    modalCompra.classList.toggle('carrito-show');
-});
+    //Boton Volver.
+    const btnVolver = document.getElementById('btn-volver');
+    btnVolver.addEventListener('click', () => {
+        div.classList.toggle('carrito-show');
+        modalCompra.classList.toggle('carrito-show');
+    });
 
 
 
-// MOSTRAR PRODUCTOS EN PANTALLA: Utilizo fetch para llamar al archivo .json y por cada objeto pinto la card dentro del main html.
-const contenedor = document.getElementById('contProductos');
-const modal = document.getElementById('contenidoCarrito');
+    // MOSTRAR PRODUCTOS EN PANTALLA: Utilizo fetch para llamar al archivo .json y por cada objeto pinto la card dentro del main html.
+    const contenedor = document.getElementById('contProductos');
+    const modal = document.getElementById('contenidoCarrito');
 
-fetch('./productos.json')
-.then(responce => responce.json())
-.then((data) => {
-    console.log(data);
-    listaProductos = data;
-    data.forEach((producto) => {
-        let {
-            id,
-            imagen,
-            nombre,
-            tipo,
-            precio,
-            desc
-        } = producto;
-        //Creo el elemento div para la card.
-        const div = document.createElement('div');
-        // Le asigno una clase 'product'
-        div.classList.add('product');
-        // inserto codigo html dentro de ese elemento div.
-        div.innerHTML = `
+    fetch('./productos.json')
+        .then(responce => responce.json())
+        .then((data) => {
+            console.log(data);
+            listaProductos = data;
+            data.forEach((producto) => {
+                let {
+                    id,
+                    imagen,
+                    nombre,
+                    tipo,
+                    precio,
+                    desc
+                } = producto;
+                //Creo el elemento div para la card.
+                const div = document.createElement('div');
+                // Le asigno una clase 'product'
+                div.classList.add('product');
+                // inserto codigo html dentro de ese elemento div.
+                div.innerHTML = `
         <div class="prod-image">
             <img src="${imagen}" alt="imagen de ${nombre}">
         </div>
@@ -124,24 +123,24 @@ fetch('./productos.json')
             </div>
         </div>
         `;
-        // Asigno el div que cree como hijo del elemento 'contenedor' (contProducts).
-        contenedor.appendChild(div);
-    
-        // Creo un evento para cada boton Agregar al Carrito.
-        const btnAgregar = document.getElementById(`agregar${id}`);
-        btnAgregar.addEventListener('click', () => {
-            // Llamo a la función agregarAlCarrito.
-            agregarAlCarrito(id);
+                // Asigno el div que cree como hijo del elemento 'contenedor' (contProducts).
+                contenedor.appendChild(div);
+
+                // Creo un evento para cada boton Agregar al Carrito.
+                const btnAgregar = document.getElementById(`agregar${id}`);
+                btnAgregar.addEventListener('click', () => {
+                    // Llamo a la función agregarAlCarrito.
+                    agregarAlCarrito(id);
+                });
+            });
         });
-    });    
-});
 
 
-// RECUPERAR DATOS DE STORAGE. Si storage tiene algo
-if (localStorage.getItem('carrito') !== null) {
-    recuperarCarrito();
-    mostrarEnCarrito();
-    contadorCarrito();
-    calcularSubTotal();
-};
+    // RECUPERAR DATOS DE STORAGE. Si storage tiene algo
+    if (localStorage.getItem('carrito') !== null) {
+        recuperarCarrito();
+        mostrarEnCarrito();
+        contadorCarrito();
+        calcularSubTotal();
+    };
 });
